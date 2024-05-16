b5a2fc5210fb0434d7518dd105f13ba25123ce6c
byehizer <ehizerjesus08@gmail.com>
2024-05-16 02:58:10 -0300

    Controlador de admin

--- src/interfaces/UserRepository.java
+++ src/interfaces/UserRepository.java
@@ -1,8 +1,21 @@
 package interfaces;
 
+import java.util.LinkedList;
 import java.util.List;
+
+import modelo.Cliente;
+
 
 
 public interface UserRepository {
-
+	
+	LinkedList<Cliente> getAllClientes(); // llama a todos los Clientes de la bdd
+    
+    Cliente getClienteByDNI(int DNI); //llama solo a uno, por su id
+    
+    void addCliente(Cliente cliente); //añade usuarios a la bdd
+    
+    void updateCliente(Cliente Cliente); //actualiza los usuarios de la bdd
+    
+    void deleteCliente(int Cliente); //eliminar usuarios de la bdd
 }
--- src/modelo/Admin.java
+++ src/modelo/Admin.java
@@ -26,11 +26,6 @@ public class Admin extends Persona {
 	public void setId_admin(int id_admin) {
 		this.id_admin = id_admin;
 	}
-
-
-	
-
-
 	
 
 
