7ffdf7c44cbbb42349494905b2c75e6436ad09be
jovalero <jrvalero2000@gmail.com>
2024-05-17 16:12:09 -0300

    Entrenador

---
+++ src/controlador/EntrenadorControlador.java
@@ -0,0 +1,104 @@
+package controlador;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+
+import javax.swing.JOptionPane;
+
+import interfaces.EntrenadorRepository;
+import modelo.Admin;
+import modelo.Cliente;
+import modelo.Entrenador;
+
+public class EntrenadorControlador implements EntrenadorRepository {
+	private static final Entrenador Entrenador = null;
+	private final Connection connection;
+
+	  public EntrenadorControlador() {
+	        this.connection = DatabaseConnection.getInstance().getConnection();
+	    
+	  }
+
+	@Override
+	public LinkedList<Entrenador> getAllEntrenadores() {
+		LinkedList<Entrenador> entrenadores= new LinkedList<Entrenador>();
+		try {
+			PreparedStatement statement= connection.prepareStatement("SELECT * FROM entrenador");
+			ResultSet resultSet= statement.executeQuery();
+			
+		while (resultSet.next()) {
+			Entrenador entrenador=new Entrenador(resultSet.getString("Nombre"),resultSet.getString("Apellido"),resultSet.getInt("Telefono"),
+					resultSet.getInt("ID_SURCUSAL"),resultSet.getInt("DNI"),resultSet.getInt("ID"),resultSet.getString("email"),resultSet.getString("Contrasenia"),resultSet.getInt("Num_Entrenados"));
+			entrenadores.add(entrenador);
+		}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Ocurrio un error al mostrar");
+		}
+		return entrenadores;
+	}
+
+	@Override
+	public Entrenador getEntrenadorByid(int id) {
+		Entrenador entrenador = null;
+	        try {
+	            PreparedStatement statement = connection.prepareStatement("SELECT * FROM entrenador WHERE id = ?");
+	            statement.setInt(1, id);
+	            
+	            ResultSet resultSet = statement.executeQuery();
+	            
+	            if (resultSet.next()) {
+	            	entrenador= new Entrenador(resultSet.getString("Nombre"),resultSet.getString("Apellido"),resultSet.getInt("Telefono"),
+	    					resultSet.getInt("ID_SURCUSAL"),resultSet.getInt("DNI"),resultSet.getInt("ID"),resultSet.getString("email"),resultSet.getString("Contrasenia"),resultSet.getInt("Num_Entrenados"));
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+		return entrenador;
+	}
+
+
+
+
+	@Override
+	public void addEntrenador(Entrenador entrenador) {
+		try {
+            PreparedStatement statement = connection.prepareStatement
+            		("INSERT INTO entrenador (ID_Entrenador, ID_sucursal, DNI, Contrasenia, Telefono, Correo_electronico, Apellido, Nombre ) "
+            				+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+          
+            statement.setInt(1, entrenador.getId_entrenador());
+            statement.setInt(2, entrenador.getId_sucursal());
+            statement.setInt(3, entrenador.getDNI());
+            statement.setString(4,entrenador.getContrasena());
+            statement.setInt(5, entrenador.getTelefono());
+            statement.setString(6,entrenador.getUsuario());
+            statement.setString(7,entrenador.getNombre());
+            statement.setString(8,entrenador.getApellido());
+            int rowsInserted = statement.executeUpdate();
+            if (rowsInserted > 0) {
+                System.out.println("Usuario insertado exitosamente");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+		
+	}
+		
+	}
+
+	@Override
+	public void updateEntrenador(Entrenador Entrenador) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteEntrenador(int Entrenador) {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ src/interfaces/EntrenadorRepository.java
@@ -0,0 +1,20 @@
+package interfaces;
+
+import java.util.LinkedList;
+
+import modelo.Entrenador;
+
+public interface EntrenadorRepository {
+	
+LinkedList<Entrenador> getAllEntrenadores(); // llama a todos los Entrenadores de la bdd
+    
+    Entrenador getEntrenadorByid(int id); //llama solo a uno, por su id
+    
+    void addEntrenador(Entrenador cliente); //añade usuarios a la bdd
+    
+    void updateEntrenador(Entrenador Entrenador); //actualiza los usuarios de la bdd
+    
+    void deleteEntrenador(int Entrenador); //eliminar usuarios de la bdd
+}
+
+
