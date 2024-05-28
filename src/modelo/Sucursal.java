f6420b3495ff2728c5ff86a90081e4ebb64db4d6
FrancoFonteiriz20 <franco.fonteiriz@davinci.edu.ar>
2024-05-27 21:50:37 -0300

    controladores

--- src/modelo/Sucursal.java
+++ src/modelo/Sucursal.java
@@ -6,7 +6,8 @@ public class Sucursal {
 	private int id_sucursal;
 	private String Direccion;
 	private int Telefono;
-	private Admin administrador;
-	private LinkedList<Cliente> clientes= new LinkedList<>();
-	private LinkedList<Entrenador> entrenadores= new LinkedList<>();
+	
+	
+	
+	
 }
