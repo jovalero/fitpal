749c77f3acb36dee415f234b02ff7074ee7424cf
martina-fasciani <martina.fasciani@davinci.edu.ar>
2024-06-25 14:59:30 -0300

    correcciones estéticas !!

--- src/vistas/HomeCliente.java
+++ src/vistas/HomeCliente.java
@@ -1,11 +1,9 @@
 package vistas;
 
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
 import modelo.Cliente;
 import javax.swing.JLabel;
 import java.awt.Font;
@@ -15,64 +13,61 @@ import java.awt.event.ActionEvent;
 
 public class HomeCliente extends JFrame {
 
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
+    private static final long serialVersionUID = 1L;
+    private JPanel contentPane;
 
-	public HomeCliente(Cliente cliente) {
-		if (cliente.getEstado_sus().equalsIgnoreCase("Nuevo")) {
-			new FormularioClienteNuevo(cliente);
-			dispose();
-		}
-		this.setVisible(true);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 837, 401);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+    public HomeCliente(Cliente cliente) {
+        if (cliente.getEstado_sus().equalsIgnoreCase("Nuevo")) {
+            new FormularioClienteNuevo(cliente);
+            dispose();
+        }
+        this.setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 673, 393);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JLabel Bienvenidolabel = new JLabel("Bienvenido " + cliente.getNombre() + " " + cliente.getApellido() +". ¿Qué desea hacer?");
-		Bienvenidolabel.setFont(new Font("Tahoma", Font.BOLD, 25));
-		Bienvenidolabel.setBounds(51, 11, 770, 64);
-		contentPane.add(Bienvenidolabel);
-		
-		JButton PerfilButton = new JButton("Ver Perfil");
-		PerfilButton.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		PerfilButton.setBounds(84, 86, 147, 53);
-		contentPane.add(PerfilButton);
-		
-		JButton CambiarRutinaButton = new JButton("Cambiar Rutina");
-		CambiarRutinaButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		CambiarRutinaButton.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		CambiarRutinaButton.setBounds(84, 161, 147, 53);
-		contentPane.add(CambiarRutinaButton);
-		
-		JButton VerProgresoButton = new JButton("Ver Progresos");
-		VerProgresoButton.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		VerProgresoButton.setBounds(84, 232, 147, 53);
-		contentPane.add(VerProgresoButton);
-		
-		JButton AnotarProgresoButton = new JButton("Anotar Progreso");
-		AnotarProgresoButton.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		AnotarProgresoButton.setBounds(427, 86, 179, 53);
-		contentPane.add(AnotarProgresoButton);
-		
-		JButton CerrarButton = new JButton("Cerrar Sesion");
-		CerrarButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		CerrarButton.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		CerrarButton.setBounds(427, 232, 179, 53);
-		contentPane.add(CerrarButton);
-		
-		JButton VerRutinabutton = new JButton("Ver Rutina");
-		VerRutinabutton.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		VerRutinabutton.setBounds(427, 161, 147, 53);
-		contentPane.add(VerRutinabutton);
-	}
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+
+        JLabel Bienvenidolabel = new JLabel("Bienvenido " + cliente.getNombre() + " " + cliente.getApellido() + ". ¿Qué desea hacer?");
+        Bienvenidolabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        Bienvenidolabel.setBounds(99, 11, 599, 38);
+        contentPane.add(Bienvenidolabel);
+
+        JButton PerfilButton = new JButton("Ver perfil");
+        PerfilButton.setBounds(52, 81, 201, 43);
+        contentPane.add(PerfilButton);
+
+        JButton CambiarRutinaButton = new JButton("Cambiar rutina");
+        CambiarRutinaButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Acción para CambiarRutinaButton
+            }
+        });
+        CambiarRutinaButton.setBounds(52, 135, 201, 43);
+        contentPane.add(CambiarRutinaButton);
+
+        JButton VerProgresoButton = new JButton("Ver progresos");
+        VerProgresoButton.setBounds(52, 189, 201, 43);
+        contentPane.add(VerProgresoButton);
+
+        JButton AnotarProgresoButton = new JButton("Anotar progreso");
+        AnotarProgresoButton.setBounds(300, 81, 201, 43);
+        contentPane.add(AnotarProgresoButton);
+
+        JButton CerrarButton = new JButton("Cerrar sesión");
+        CerrarButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Acción para CerrarButton
+            }
+        });
+        CerrarButton.setBounds(300, 135, 201, 43);
+        contentPane.add(CerrarButton);
+
+        JButton VerRutinabutton = new JButton("Ver rutina");
+        VerRutinabutton.setBounds(300, 189, 201, 43);
+        contentPane.add(VerRutinabutton);
+    }
 }
+
--- src/vistas/HomeClientePremium.java
+++ src/vistas/HomeClientePremium.java
@@ -1,89 +1,72 @@
 package vistas;
 
 import java.awt.EventQueue;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
 import modelo.Cliente;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class HomeClientePremium extends JFrame {
 
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
+    private static final long serialVersionUID = 1L;
+    private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
+    public HomeClientePremium(Cliente cliente) {
+        this.setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 673, 393);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPane.setLayout(null);
 
+        JLabel Bienvenidolabel = new JLabel("Bienvenido " + cliente.getNombre() + " " + cliente.getApellido() + ". ¿Qué desea hacer?");
+        Bienvenidolabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        Bienvenidolabel.setBounds(99, 11, 599, 38);
+        contentPane.add(Bienvenidolabel);
 
-	/**
-	 * Create the frame.
-	 */
-	public HomeClientePremium(Cliente cliente) {
-		this.setVisible(true);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 588, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(null);
-		
-		JLabel Bienvenidolabel = new JLabel("Bienvenido " + cliente.getNombre() + " " + cliente.getApellido() +". ¿Qué desea hacer?");
-		Bienvenidolabel.setFont(new Font("Tahoma", Font.BOLD, 25));
-		Bienvenidolabel.setBounds(43, 10, 691, 31);
-		contentPane.add(Bienvenidolabel);
+        JButton PerfilButton = new JButton("Ver perfil");
+        PerfilButton.setBounds(52, 81, 201, 43);
+        contentPane.add(PerfilButton);
 
-		JButton PerfilButton = new JButton("Ver Perfil");
-		PerfilButton.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		PerfilButton.setBounds(64, 80, 99, 29);
-		contentPane.add(PerfilButton);
-		
-		JButton VerProgresoButton = new JButton("Ver Progresos");
-		VerProgresoButton.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		VerProgresoButton.setBounds(64, 120, 135, 29);
-		contentPane.add(VerProgresoButton);
-		
-		JButton AnotarProgresoButton = new JButton("Anotar Progreso");
-		AnotarProgresoButton.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		AnotarProgresoButton.setBounds(64, 162, 153, 29);
-		contentPane.add(AnotarProgresoButton);
-		
-		JButton CerrarButton = new JButton("Cerrar Sesion");
-		CerrarButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		CerrarButton.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		CerrarButton.setBounds(362, 206, 131, 29);
-		contentPane.add(CerrarButton);
-		
-		JButton VerRutinabutton = new JButton("Ver Rutina");
-		VerRutinabutton.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		VerRutinabutton.setBounds(64, 206, 111, 29);
-		contentPane.add(VerRutinabutton);
-		
-		JButton CanjearIncentivo = new JButton("Canjear Incetivos");
-		CanjearIncentivo.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		CanjearIncentivo.setBounds(350, 80, 161, 29);
-		contentPane.add(CanjearIncentivo);
-		
-		JButton btnVerDieta = new JButton("Ver Dieta");
-		btnVerDieta.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		btnVerDieta.setBounds(350, 120, 161, 29);
-		contentPane.add(btnVerDieta);
-		
-		JButton btnVerEntrenador = new JButton("Ver Entrenador");
-		btnVerEntrenador.setFont(new Font("Tahoma", Font.PLAIN, 17));
-		btnVerEntrenador.setBounds(350, 162, 161, 29);
-		contentPane.add(btnVerEntrenador);
-		setContentPane(contentPane);
-	}
-		
+        JButton VerProgresoButton = new JButton("Ver progresos");
+        VerProgresoButton.setBounds(52, 135, 201, 43);
+        contentPane.add(VerProgresoButton);
 
+        JButton AnotarProgresoButton = new JButton("Anotar progreso");
+        AnotarProgresoButton.setBounds(52, 189, 201, 43);
+        contentPane.add(AnotarProgresoButton);
+
+        JButton VerRutinabutton = new JButton("Ver rutina");
+        VerRutinabutton.setBounds(52, 243, 201, 43);
+        contentPane.add(VerRutinabutton);
+
+        JButton CanjearIncentivo = new JButton("Canjear incentivos");
+        CanjearIncentivo.setBounds(300, 81, 201, 43);
+        contentPane.add(CanjearIncentivo);
+
+        JButton btnVerDieta = new JButton("Ver dietas");
+        btnVerDieta.setBounds(300, 135, 201, 43);
+        contentPane.add(btnVerDieta);
+
+        JButton btnVerEntrenador = new JButton("Ver entrenador");
+        btnVerEntrenador.setBounds(300, 189, 201, 43);
+        contentPane.add(btnVerEntrenador);
+
+        JButton CerrarButton = new JButton("Cerrar sesión");
+        CerrarButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Acción para CerrarButton
+            }
+        });
+        CerrarButton.setBounds(300, 243, 201, 43);
+        contentPane.add(CerrarButton);
+
+        setContentPane(contentPane);
+    }
 }
+
