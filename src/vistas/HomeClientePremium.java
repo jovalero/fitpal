6d5818e7959789102f53901e6eea61240b80632d
byehizer <ehizerjesus08@gmail.com>
2024-06-25 09:25:53 -0300

    =?

--- src/vistas/HomeClientePremium.java
+++ src/vistas/HomeClientePremium.java
@@ -29,7 +29,7 @@ public class HomeClientePremium extends JFrame {
 	public HomeClientePremium(Cliente cliente) {
 		this.setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 588, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(null);
